ceb882eeb6c2ab676b0a4d7875c285ca2420b5a0
Yichen Mo <amomorning@gmail.com>
2020-08-13 13:39:03 +0800

    Init repo

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# seu-autoenter
-东南大学每日报平安健康打卡及入校申请自动化程序
+# SEU AutoEnter
+
+[![license](https://img.shields.io/github/license/amomorning/seu-autoenter)](https://github.com/amomorning/seu-autoenter/blob/master/LICENSE)
+
+东南大学每日报平安健康打卡及入校申请自动化程序  
+
+登录程序主体来自 [StephenHoo/AutoLogin](https://github.com/StephenHoo/AutoLogin)
+
+## Requirements
+- python3
+- selenium
+- chrome (测试过程使用的版本号为 84.0.4147.125 (Official Build) (64-bit))
+
+## Usage
+``` bash
+conda create -n selenium
+conda activate selenium
+conda install -c conda-forge selenium
+
+# 每日打卡
+start /b python login_chrome.py
+[输入用户名密码]
+
+# 申请入校
+start /b python enter_chrome.py
+```
+
+## FAQ
+#### Q: 为什么需要这个程序?
+A: 经常在中午12点超出一丢丢的时候意识到妹油申请入校 TAT 原先可以手动调本地时间避开申请时间限制，但是似乎在某此更新里学校修复了这个bug....
---
+++ enter_chrome.py
@@ -0,0 +1,237 @@
+# coding=utf-8
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By #按照什么方式查找，By.ID,By.CSS_SELECTOR
+from selenium.webdriver.common.keys import Keys #键盘按键操作
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait #等待页面加载某些元素
+from selenium.webdriver.chrome.options import Options
+from datetime import date, timedelta
+import time
+import random
+
+
+# 加启动配置 禁用日志log
+chrome_options = Options()
+chrome_options.add_argument('–no-sandbox')# “–no - sandbox”参数是让Chrome在root权限下跑
+chrome_options.add_argument('–disable-dev-shm-usage')
+chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
+chrome_options.add_argument('--start-maximized')#最大化
+chrome_options.add_argument('--incognito')#无痕隐身模式
+chrome_options.add_argument("disable-cache")#禁用缓存
+chrome_options.add_argument('log-level=3')
+chrome_options.add_argument('disable-infobars')
+chrome_options.add_argument('--headless')
+
+url = "https://newids.seu.edu.cn/authserver/login?service=http://ehall.seu.edu.cn/qljfwapp3/sys/lwWiseduElectronicPass/*default/index.do"
+dailyDone = False # 今日是否已经打卡
+address = '前工院南602'
+
+# 创建打卡记录log文件
+def writeLog(text):
+    with open('log.txt', 'a') as f:
+        s = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ' ' + text
+        print(s)
+        f.write(s + '\n')
+        f.close()
+
+
+
+def enterUserPW():
+    # 创建账号密码文件，以后都不用重复输入
+    # 1.1版本之后更新可以读取 chrome.exe 的位置，防止用户Chrome浏览器未安装到默认位置导致的程序无法执行
+    try:
+        with open("loginData.txt", mode='r', encoding='utf-8') as f:
+            # 去掉换行符
+            lines = f.readlines()
+            user = lines[0].strip()
+            pw = lines[1].strip()
+            if len(lines) > 2:
+                loc = lines[2].strip()
+            else:
+                loc = ""
+            f.close()
+    except FileNotFoundError:
+        print("Welcome to AUTO DO THE F***ING DAILY JOB, copyright belongs to S.H.")
+        with open("loginData.txt", mode='w', encoding='utf-8') as f:
+            user = input('Please Enter Your Username: ')
+            pw = input('Then Please Enter Your Password: ')
+            loc = ""
+            f.write(user + '\n')
+            f.write(pw + '\n')
+            f.close()
+
+    return user, pw, loc
+
+
+def login(user, pw, browser):
+    browser.get(url)
+    browser.implicitly_wait(1)
+    
+    # 填写用户名密码
+    username = browser.find_element_by_id('username')
+    password = browser.find_element_by_id('password')
+    username.clear()
+    password.clear()
+    username.send_keys(user)
+    password.send_keys(pw)
+
+
+    # 点击登录
+    login_button = browser.find_element_by_class_name('auth_login_btn')
+    login_button.submit()
+
+# 检查是否无text按钮
+def check(text, browser):
+    buttons = browser.find_elements_by_class_name(text)
+    if(len(buttons) > 0):
+        return True
+    return False
+    
+def click_select(id, pos, browser):
+    reqid = "document.getElementsByClassName('is-require')[" + id + "]"
+    # click
+    js = reqid + ".click();"
+    print(js)
+    browser.executeScript(js)
+    time.sleep(1)
+
+    # select pos
+    js = reqid + ".parentElement.getElementsByClassName('mt-picker-column-item')[" + pos + "].click();"
+    browser.executeScript(js)
+    print(js)
+    time.sleep(1)
+
+    # confirm
+    js = reqid + ".getElementsByClassName('mint-picker__confirm')[0].click();"
+    browser.executeScript(js)
+    print(js)
+    time.sleep(1)
+
+
+def click_checkbox(id, browser):
+    reqid = "document.getElementsByClassName('is-require')[" + id + "]"
+
+    # click checkbox
+    js = reqid + ".parentElement.getElementsByTagName('input')[1].click();"
+    browser.executeScript(js)
+    print(js)
+    time.sleep(1)
+
+    # confirm
+    js = reqid + ".parentElement.getElementsByClassName('mint-selected-footer-confirm')[0].click();"
+    browser.executeScript(js)
+    print(js)
+    time.sleep(1)
+
+
+def click_enter_date(id, dd, hh, mm, browser):
+    reqid = "document.getElementsByClassName('is-require')[" + id + "]"
+
+    # click 
+    js = reqid + ".click();"
+    print(js)
+    browser.executeScript(js)
+    time.sleep(1)
+
+    # set day
+    js = reqid + "parentElement.getElementsByClassName('mint-picker-column')[2].getElementsByClassName('mt-picker-column-item')[" + dd + "].click()"
+    browser.executeScript(js)
+    print(js)
+    time.sleep(1)
+
+    # set hour
+    js = reqid + "parentElement.getElementsByClassName('mint-picker-column')[3].getElementsByClassName('mt-picker-column-item')[" + hh + "].click()"
+    browser.executeScript(js)
+    print(js)
+    time.sleep(1)
+
+    # set minite
+    js = reqid + "parentElement.getElementsByClassName('mint-picker-column')[4].getElementsByClassName('mt-picker-column-item')[" + mm + "].click()"
+    browser.executeScript(js)
+    print(js)
+    time.sleep(1)
+
+    # confirm
+    js = reqid + ".getElementsByClassName('mint-picker__confirm')[0].click();"
+    browser.executeScript(js)
+    print(js)
+    time.sleep(1)
+
+
+def input_address(id, browser):
+    reqid = "document.getElementsByClassName('is-require')[" + id + "]"
+    # click 
+    js = reqid + ".getElementsByTagName('input')[0].innerText = '" + address + "';"
+    print(js)
+    browser.executeScript(js)
+    time.sleep(1)
+
+
+if __name__ == "__main__":
+    user, pw, browser_loc = enterUserPW()
+    # 判断是否写入非默认安装位置的 Chrome 位置
+    if len(browser_loc) > 10:
+        chrome_options.binary_location = browser_loc
+    
+    localtime = time.localtime(time.time())
+    set_minite = localtime.tm_min # 首次登陆的分钟时刻，代表以后每次在此分钟时刻打卡
+    set_hour = localtime.tm_hour # 首次登陆的时钟时刻，代表以后每次在此时钟时刻打卡
+
+    if set_hour > 9:
+        set_hour = 7 # 如果首次登录超过上午10点，则以后默认在7点钟打卡
+        first_time = True
+
+    while True:
+        try:
+            # 登录打卡一次试一试
+            browser = webdriver.Chrome('./chromedriver',options=chrome_options)
+            print("------------------浏览器已启动----------------------")
+            login(user, pw, browser)
+            browser.implicitly_wait(1)
+            time.sleep(10)
+
+            # 确认是否打卡成功
+            # 的确无新增按钮
+            dailyDone = not check("mint-fixed-button", browser)
+            print(browser.current_url)
+            if dailyDone is True and check("退出", browser) is True: # 今日已完成打卡
+                sleep_time = (set_hour+24-time.localtime(time.time()).tm_hour)*3600 + (set_minite-time.localtime(time.time()).tm_min)*60
+                writeLog("下次打卡时间：明天" + str(set_hour) + ':' + str(set_minite) + "，" + "即" + str(sleep_time) + 's后')
+                browser.quit()
+                print("------------------浏览器已关闭----------------------")
+                time.sleep(sleep_time)
+            elif dailyDone is False: # 今日未完成打卡
+                # 点击报平安
+                buttons = browser.find_element_by_class_name('mint-fixed-button')
+                for button in buttons:
+                    button.click()
+                    browser.implicitly_wait(10)
+                    print(browser.current_url)
+
+                    for i in range(5, 8):
+                        select(i, 0, browser)
+                    
+                    # TODO 到校方式
+
+                    click_checkbox(10, browser)
+
+                    tomorrow = (date.today() + timedelta(1)).day
+
+                    click_enter_date(11, tomorrow, 8, 31, browser)
+                    click_enter_date(12, tomorrow, 21, 59, browser)
+
+                    # TODO test input function
+                    input_address(13, browser)
+
+                    select(15, 2, browser)
+
+
+            else:
+                browser.close()
+                print("------------------网站出现故障----------------------")
+                print("------------------浏览器已关闭----------------------")
+                time.sleep(1) # 昏睡5min 为了防止网络故障未打上卡
+        except Exception as r:
+            print("未知错误 %s" %(r))
